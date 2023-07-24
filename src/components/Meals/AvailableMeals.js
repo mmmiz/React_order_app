461ac945e1200dbef4db0d02ca31a8bbb8cf4e82
mmmiz <miduho4soft@yahoo.co.jp>
2023-07-24 11:33:25 -0500

    Your commit message

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,52 @@
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
+import classes from './AvailableMeals.module.css';
+
+const DUMMY_MEALS = [
+  {
+    id: 'm1',
+    name: 'Sushi',
+    description: 'Japanese salmon roll sushi',
+    price: 22.99,
+  },
+  {
+    id: 'm2',
+    name: 'Spaghetti',
+    description: 'A special spaghetti!',
+    price: 16.5,
+  },
+  {
+    id: 'm3',
+    name: 'Barbecue Burger',
+    description: 'American, raw, meaty',
+    price: 12.99,
+  },
+  {
+    id: 'm4',
+    name: 'Salad',
+    description: 'Healthy Salad',
+    price: 18.99,
+  },
+];
+
+const AvailableMeals = () => {
+  const mealsList = DUMMY_MEALS.map((meal) => (
+    <MealItem
+      key={meal.id}
+      id={meal.id}
+      name={meal.name}
+      description={meal.description}
+      price={meal.price}
+    />
+  ));
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>{mealsList}</ul>
+      </Card>
+    </section>
+  );
+};
+
+export default AvailableMeals;
---
+++ src/components/Meals/MealsSummary.js
@@ -0,0 +1,12 @@
+import classes from './MealsSummary.module.css';
+
+const MealsSummary = () => {
+  return (
+    <section className={classes.summary}>
+      <h2>Delicious Food, Delivered To You</h2>
+        <p>Delicious, warm, and easy food!</p>
+    </section>
+  );
+};
+
+export default MealsSummary;
